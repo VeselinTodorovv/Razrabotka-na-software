41c6cef797001a724a867b3b50a33624a983d66c
HolyRNJesus <veskotodorov786@protonmail.com>
2023-04-27 11:33:25 +0300

    cats

---
+++ FDMCApp/FDMCApp/Models/Cat.cs
@@ -0,0 +1,11 @@
+namespace FDMCApp.Models
+{
+    public class Cat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
