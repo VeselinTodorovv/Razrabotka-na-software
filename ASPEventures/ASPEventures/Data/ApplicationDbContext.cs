c2f2d5b47cf924d70e01024150915b3a63f08b67
HolyRNJesus <veskotodorov786@protonmail.com>
2023-06-05 09:28:34 +0300

    eventures

---
+++ ASPEventures/ASPEventures/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ASPEventures.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASPEventures.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<EventuresUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
